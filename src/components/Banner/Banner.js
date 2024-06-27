b384b36b982b2cbe4eae46aeebe14b7e45b739a5
Lázaro Santos <98424392+lazarosantos1011@users.noreply.github.com>
2024-06-27 10:21:36 -0300

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css';
 export const Banner = () => {
     return (
         <header className="banner">
-        <img src="/images/banner.png" alt="Banner principal da página do Organo"/>
+            <img src="/images/banner.png" alt="Banner principal da página do Organo"/>
         </header>
     )
 }
